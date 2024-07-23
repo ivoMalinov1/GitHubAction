2e1feb3dc43c76cabf2384cacc6c517f01a8ee0a
Ivo Malinov <ii.malinov@tinqin.com>
2024-07-23 14:42:11 +0300

    introduce bug

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+# # app.py
+# def add(a, b):
+#     return a + b
+#
+# if __name__ == "__main__":
+#     print(add(1, 2))
+
+
 # app.py
 def add(a, b):
-    return a + b
+    return a - b  # This is a deliberate bug
 
 if __name__ == "__main__":
     print(add(1, 2))
